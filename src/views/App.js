1672b41633fa7c0c6a53b24c8a1c8e666810ad8a
TRAN TUAN ANH <trantuananh46hn@gmail.com>
2023-11-19 14:59:52 +0700

    video7

--- src/views/App.js
+++ src/views/App.js
@@ -1,18 +1,21 @@
 import logo from './logo.svg';
 import './App.scss';
+import Nav from './Nav.js'
 const App = () => {
   let name = "Eric";
-  let number = 2021
-  let object = { name: `Tuan Anh`, age: `23` }
-  let link = `https://viettuts.vn/java-string/lop-stringbuilder-trong-java`;
+
+  const handleEventClick = (event) => {
+    console.log(">>>>>>>>> click me ", event.target.value)
+  }
+
   return (
     <div className="App">
-      {console.log(`check object : `, object)}
+      <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and {name} in {number}!</h1>
-        <p style={{ color: "red", fontSize: "20px", marginTop: "15px" }}>Hello Newbie</p>
-        <a href={link} target='_blank'>Click here</a>
+        <h1>Hello world with React and {name}!</h1>
+        <input type="text" value={"Erric"} onClick={(event) => { handleEventClick(event) }}></input>
+        <button type="button" onClick={(event) => { handleEventClick(event) }}>Click me</button>
       </header>
     </div>
   );
